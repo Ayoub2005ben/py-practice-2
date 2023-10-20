308a4634f6fbbcf4a32bf9bc119ab0b10384900d
CarolinaCasGil <113991051+CarolinaCasGil@users.noreply.github.com>
2023-10-20 13:22:02 +0200

    ejercicio 1

--- ex1.py
+++ ex1.py
@@ -4,13 +4,20 @@ Exercices for input
 
 # 1. Ask the user for his/her name
 name = input("What is your name? ")
-
+print(name)
 # 2. Ask the user for his/her age
-
+age = input("What is your age? ")
+print(age)
 # 3. Ask the user for his/her favorite_color
-
+favorite_color = input("What is your favorite color? ")
+print(favorite_color)
 # 4. Ask the user for his/her favorite_number
-
+favorite_number = input("what is your favorite number? ")
+print(favorite_number)
 # 5. Print out the information in a sentence
+sentence = input("name, age, favorite_color, favorite_number? ")
+print(sentence)
 
 # 6. Multiply the user's favorite number by 10 and print out the result
+operation = float( number )*10
+print(50)
