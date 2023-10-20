5b7d6f509d76c6715eaa7c2fa0a65d98e8c19846
CarolinaCasGil <113991051+CarolinaCasGil@users.noreply.github.com>
2023-10-20 13:59:36 +0200

    ejercicio 3

--- ex3.py
+++ ex3.py
@@ -8,16 +8,44 @@ import random
 # "rps" means rock, paper, scissors
 rps = ['rock','paper','scissors']
 
+
+
 # 2. Create a variable called user_choice that takes input from the user and stores it.
 #   Make sure the user enters rock, paper, or scissors.
 user_choice = "" # fix this, user input, hint: use input("...")
+user_choice = input("Enter your choice (rock, paper, or scissors): ").lower()
+
+while user_choice not in ["rock", "paper", "scissors"]:
+    user_choice = input("Invalid choice. Please enter rock, paper, or scissors: ").lower()
+
+print("You chose:", user_choice)
+
+
 
 # 3. Create a variable called computer_choice that randomly chooses from rps.
 computer_choice = "" # fix this, random choice from rps
+import random
+
+rps = ["rock", "paper", "scissors"]
+computer_choice = random.choice(rps)
 
 # 4. Print out the user_choice and the computer_choice.
 print("You chose: " + user_choice)
 print("The computer chose: " + computer_choice)
+import random
+
+rps = ["rock", "paper", "scissors"]
+
+user_choice = input("Enter your choice (rock, paper, or scissors): ").lower()
+
+while user_choice not in rps:
+    user_choice = input("Invalid choice. Please enter rock, paper, or scissors: ").lower()
+
+computer_choice = random.choice(rps)
+
+print("User's choice: " + user_choice)
+print("Computer's choice: " + computer_choice)
+
 
 # 5. Print out the winner of rock, paper, scissors.
 # continue this if statement to print out the winner
@@ -42,3 +70,23 @@ if user_choice == computer_choice:
 #   print("You are close!")
 # else:
 #   print("You are wrong!")
+import random
+
+rps = ["rock", "paper", "scissors"]
+
+user_choice = input("Enter your choice (rock, paper, or scissors): ").lower()
+
+while user_choice not in rps:
+    user_choice = input("Invalid choice. Please enter rock, paper, or scissors: ").lower()
+
+computer_choice = random.choice(rps)
+
+print("User's choice: " + user_choice)
+print("Computer's choice: " + computer_choice)
+
+if user_choice == computer_choice:
+    print("It's a tie!")
+elif (user_choice == "rock" and computer_choice == "scissors") or (user_choice == "paper" and computer_choice == "rock") or (user_choice == "scissors" and computer_choice == "paper"):
+    print("You win!")
+else:
+    print("You lose!")
